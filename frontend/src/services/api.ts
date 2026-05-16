2a87dab7583204edae094186aa1923be746e1c01
k-k-0 <aadityyadav7531@gmail.com>
2026-05-16 15:03:37 +0530

    deploying frontend with deployed backend url

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios, { type InternalAxiosRequestConfig } from "axios";
 const API = axios.create({
-    baseURL:  "http://127.0.0.1:8000"
+    baseURL:  "https://civic-ai-chz7.onrender.com"
 })
 
 API.interceptors.request.use((req: InternalAxiosRequestConfig ): InternalAxiosRequestConfig => {
