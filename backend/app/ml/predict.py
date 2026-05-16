5efcf0116b23c97a9512716d70b7dd098db92b53
k-k-0 <aadityyadav7531@gmail.com>
2026-05-16 12:04:51 +0530

    Fine tune my machine learning model for sentiments and did some changes in frontend

--- backend/app/ml/predict.py
+++ backend/app/ml/predict.py
@@ -6,11 +6,56 @@ priority_model = joblib.load("app/ml/priority_model.pkl")
 
 sentiment_model = joblib.load("app/ml/sentiment_model.pkl")
 
+POSITIVE_WORDS = [
+    "planted",
+    "clean",
+    "improved",
+    "successful",
+    "good",
+    "better",
+    "green",
+    "restored",
+    "fixed",
+    "maintained"
+]
+
+NEGATIVE_WORDS = [
+    "broken",
+    "garbage",
+    "damage",
+    "pollution",
+    "problem",
+    "dirty",
+    "overflowing",
+    "leakage",
+    "dangerous",
+    "traffic"
+]
+
+def detect_sentiment(text):
+
+    text = text.lower()
+
+    positive_score = sum(
+        word in text for word in POSITIVE_WORDS
+    )
+
+    negative_score = sum(
+        word in text for word in NEGATIVE_WORDS
+    )
+
+    if positive_score > negative_score:
+        return "Positive"
+
+    return "Negative"
+
 def predict_complaint(text: str):
+
+    text = text.lower()
 
     category = category_model.predict([text])[0]
     priority = priority_model.predict([text])[0]
-    sentiment = sentiment_model.predict([text])[0]
+    sentiment = detect_sentiment(text)
 
     return {
         "category": category,
--- backend/app/ml/train_model.py
+++ backend/app/ml/train_model.py
@@ -6,23 +6,30 @@ import joblib
 
 df = pd.read_csv(r"/home/k-k/civicAI/backend/app/ml/complaints.csv")
 
+df['text'] = df['text'].str.lower()
+
+print(df['category'].value_counts())
+print(df['priority'].value_counts())
+print(df['sentiment'].value_counts())
+
+
 category_model = Pipeline([
-    ('tfidf', TfidfVectorizer()),
-    ('model', LogisticRegression())
+    ('tfidf', TfidfVectorizer(ngram_range=(1,2), stop_words='english')),
+    ('model', LogisticRegression(max_iter=1000))
 ])
 
 category_model.fit(df['text'], df['category'])
 
 priority_model = Pipeline([
-    ('tfidf', TfidfVectorizer()),
-    ('model', LogisticRegression())
+    ('tfidf', TfidfVectorizer(ngram_range=(1,2), stop_words='english')),
+    ('model', LogisticRegression(max_iter=1000))
 ])
 
 priority_model.fit(df['text'], df['priority'])
 
 sentiment_model = Pipeline([
-    ('tfidf', TfidfVectorizer()),
-    ('model', LogisticRegression())
+    ('tfidf', TfidfVectorizer(ngram_range=(1,2), stop_words='english')),
+    ('model', LogisticRegression(max_iter=1000))
 ])
 
 sentiment_model.fit(df['text'], df['sentiment'])
@@ -34,3 +41,21 @@ joblib.dump(priority_model, "backend/app/ml/priority_model.pkl")
 joblib.dump(sentiment_model, "backend/app/ml/sentiment_model.pkl")
 
 print('Model trained successfully')
+
+print(
+    category_model.predict(
+        ["road broken badly"]
+    )
+)
+
+print(
+    category_model.predict(
+        ["garbage everywhere"]
+    )
+)
+
+print(
+    category_model.predict(
+        ["trees planted successfully"]
+    )
+)
